6f083ca4741462cb1d48bd5116552e77fdd8c571
Andres Alban <bethesdas101@gmail.com>
2020-11-03 17:16:11 -0500

    Adding zoo animals data for endpoints

---
+++ db/data.js
@@ -0,0 +1,66 @@
+const data = {
+    animals: [
+        {
+            id: 1,
+            family: 'feline',
+            specie: 'lion',
+            name: 'Patrice',
+            imageUrl: 'https://images.pexels.com/photos/1187987/pexels-photo-1187987.jpeg?cs=srgb&dl=pexels-aldo-picaso-1187987.jpg&fm=jpg',
+            age: 7,
+            active: false,
+            medical_status: 'broken knee'
+        },
+        {
+            id: 2,
+            family: 'feline',
+            specie: 'lion',
+            name: 'Selina',
+            imageUrl: 'https://images.pexels.com/photos/325976/pexels-photo-325976.jpeg?cs=srgb&dl=pexels-pixabay-325976.jpg&fm=jpg',
+            age: 6,
+            active: false,
+            medical_status: 'teeth injured'
+        },
+        {
+            id: 3,
+            family: 'Giraffidae',
+            specie: 'giraffe',
+            name: 'Ted',
+            imageUrl: 'https://images.pexels.com/photos/39504/giraffe-animal-funny-facial-expression-39504.jpeg?cs=srgb&dl=pexels-pixabay-39504.jpg&fm=jpg',
+            age: 14,
+            active: false,
+            medical_status: 'medium cut in neck'
+        },
+        {
+            id: 4,
+            family: 'Ursidae',
+            specie: 'bear',
+            name: 'ursa',
+            imageUrl: 'https://images.pexels.com/photos/3634926/pexels-photo-3634926.jpeg?cs=srgb&dl=pexels-janko-ferlic-3634926.jpg&fm=jpg',
+            age: 12,
+            active: false,
+            medical_status: 'unstable digestion'
+        },
+        {
+            id: 5,
+            family: 'Ursidae',
+            specie: 'bear',
+            name: 'matilde',
+            imageUrl: 'https://images.pexels.com/photos/3048718/pexels-photo-3048718.jpeg?cs=srgb&dl=pexels-vincent-ma-janssen-3048718.jpg&fm=jpg',
+            age: 12,
+            active: false,
+            medical_status: 'back surgery'
+        },
+        {
+            id: 6,
+            family: 'feline',
+            specie: 'siberian tiger',
+            name: 'leonidas',
+            imageUrl: 'https://images.pexels.com/photos/247615/pexels-photo-247615.jpeg?cs=srgb&dl=pexels-pixabay-247615.jpg&fm=jpg',
+            age: 9,
+            active: false,
+            medical_status: 'cut in tail'
+        }
+    ]
+}
+
+module.exports = data;
