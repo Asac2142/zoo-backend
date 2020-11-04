db0bf2d5c46eae95d1ed78949ad6371bdd070b31
Andres Alban <bethesdas101@gmail.com>
2020-11-04 00:02:26 -0500

    Updating animal schema in name and imageUrl properties

--- schema/schema.js
+++ schema/schema.js
@@ -4,8 +4,8 @@ const schema = Joi.object({
     id: Joi.string().required(),
     family: Joi.string().required(),
     specie: Joi.string().required(),
-    name: Joi.string().alphanum().min(3).max(15).required(),
-    imageUrl: Joi.object(),
+    name: Joi.string().min(3).max(15).required(),
+    imageUrl: Joi.string(),
     age: Joi.number().greater(0).required(),
     active: Joi.boolean().required(),
     medical_status: Joi.string().required()
